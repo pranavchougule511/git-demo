d6fe77cab340f4bb2b77b68c523b6db4d80f4dc7
sankey-omkar <omkar.m1@sankeysolutions.com>
2025-09-19 11:34:29 +0530

    Revert "add one change"
    This reverts commit 175dc7051c3f71fb2f5f4e4d3f17494865618659.

--- src/App.js
+++ src/App.js
@@ -10,8 +10,7 @@ function App() {
           Edit <code>src/App.js</code> and save to reload.          
         </p>
         <p className='subHeading'>
-          Hey, this is Git Session & today is Tuesday 26th August. <br/>
-          And it's a beautiful sunny Day.
+          hthis ajsfo osadhfo 
         </p>
 
         <button>Click Here!</button>
